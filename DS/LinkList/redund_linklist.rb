f4106948cecce7bf61f41b7ff376245c81b8e822
naveenkumar007 <naveenkumar0500@gmail.com>
2019-01-09 01:25:20 +0530

    initialize structire

---
+++ DS/LinkList/redund_linklist.rb
@@ -0,0 +1,16 @@
+require "./linklist"
+
+
+class ReduntLinklist
+	def initialize()
+      @ll = Linklist.new
+    end
+end
+
+module LLMethod
+  def Create(arr)
+  	arr.each do ||
+  	end	
+    puts "abc"
+  end
+end
